dc37d2fed4c7dd464decc9a5cbb99482135e6308
Seamus <sdmoc@iinet.net.au>
2026-01-19 17:06:27 +1100

    Created index.html and script.js files for Rock Paper Scissors game. index.html sets up the basic HTML structure and links to script.js. script.js will contain the game logic written code in script.js to log computer input to game

---
+++ script.js
@@ -0,0 +1,20 @@
+// Script for rock paper scissors game
+// understanding the problem, breaking it down, designing a step-by-step solution (algorithm/pseudocode), 
+// coding it, testing rigorously, debugging errors, and documenting the process
+
+ console.log("Hello, Rock Paper Scissors!");
+
+ function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+console.log(getRandomArbitrary(0, 3));
+
+function computerInput(randomNumber) {
+    if (randomNumber < 1)
+    return ("Rock");
+    if (randomNumber >= 1 && randomNumber < 2)
+        return ("Paper");
+    if (randomNumber >= 2)
+        return ("Scissors");
+}
+console.log(computerInput(getRandomArbitrary(0, 3)));
